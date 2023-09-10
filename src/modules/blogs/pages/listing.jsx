9d3953ab674e1e275b09b45bfeb3cc993ba3a50f
Farah Nasreen <farahnasreen2000@gmail.com>
2023-09-10 10:21:37 +0530

    Impl blog layout

--- src/modules/blogs/pages/listing.jsx
+++ src/modules/blogs/pages/listing.jsx
@@ -1,54 +1,55 @@
-import React, { useState } from "react";
-// import Card from "../../../common/components/card";
-import Masonry from "react-masonry-css";
-import "../../../common/styles/global.css";
-import { Button, Card, Container, Modal } from "react-bootstrap";
+import React from "react";
+import { Container, Row, Col, Card } from "react-bootstrap";
+import { blogsDummyData } from "../blog-data/blog-data";
+import '../styles/blogStyles.css';
 
 const BlogListing = () => {
   return (
     <>
-      <Container className="alert alert-success w-100">
-        <h1 className="page-title">Blogs</h1>
-        <p>
-          Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industry's standard dummy text ever
-          since the 1500s, when an unknown printer took a galley of type and
-          scrambled it to make a type specimen book.{" "}
-        </p>
-      </Container>
-      <Container className="w-100">
-        <Masonry
-          breakpointCols={{
-            default: 4,
-            1100: 3,
-            700: 2,
-            500: 1,
-          }}
-          className="my-masonry-grid"
-          columnClassName="my-masonry-grid_column"
-        >
-          {[1, 2, 3, 4, 5, 6, 7, 8, 9].map((_i) => (
-            <Card className="bg-light cursor-pointer">
-              <Card.Body>
-                <Card.Title>Card title</Card.Title>
-                <Card.Text>Last updated 3 mins ago</Card.Text>
-                <Card.Img
-                  variant="top"
-                  src="https://picsum.photos/200/300"
-                  height={300}
-                />
-                <Card.Text className="text-truncate">
-                  This is a longer card with supporting text below as a natural
-                  lead-in to additional content. This content is a little bit
-                  longer.
-                </Card.Text>
-              </Card.Body>
-            </Card>
-          ))}
-        </Masonry>
+      <Container className="my-4 w-100">
+        <Row>
+          {
+            blogsDummyData.map((item, index) => (
+              !(index % 2) ? (
+                <>
+                  <Col md={6} >
+                    <Card className="shadow" >
+                      <Card.Img
+                        variant="top"
+                        src={item.image}
+                        height={200}
+                        width={200}
+                      />
+                    </Card>
+                  </Col>
+                  <Col md={6} className="mt-4">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum suscipit ullam rem nemo dolores. Doloremque aliquam labore facere blanditiis corrupti odio dolorem, atque fuga sit, id doloribus non. Molestias, quos?</p>
+                  </Col>
+                </>
+              ) : (
+                <>
+                  <Col md={6} className="mt-4">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit, reprehenderit vero ea debitis libero quod modi distinctio at consequatur labore, doloremque eius, dolore rerum quos rem adipisci cum. Corporis, adipisci!</p>
+                  </Col>
+                  <Col md={6}>
+                    <Card className="shadow" >
+                      <Card.Img
+                        variant="top"
+                        src={item.image}
+                        height={200}
+                        width={200}
+                      />
+                    </Card>
+                  </Col>
+                </>
+              )
+            ))
+          }
+        </Row>
       </Container>
     </>
-  );
-};
+  )
+}
 
-export default BlogListing;
+export default BlogListing
+
