1c49e750c3316f98cbd58f912c28c7e8aa835d7c
Farah Nasreen <farahnasreen2000@gmail.com>
2023-09-10 10:20:09 +0530

    Add static blog-data

---
+++ src/modules/blogs/blog-data/blog-data.js
@@ -0,0 +1,23 @@
+
+export const blogsDummyData = [
+    {
+        image: "https://jpeg.org/images/jpeg-home.jpg",
+        descripton: "This is the first description of the image"
+    },
+    {
+        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3a/Cat03.jpg/1025px-Cat03.jpg",
+        descripton: "This is the second description of the image"
+    },
+    {
+        image: "https://drive.google.com/uc?export=view&id=1kAJi5hbKGibYmRmIjs64lCv3aqOlB_Ad",
+        descripton: "This is the thirs description of the image"
+    },
+    {
+        image: "https://drive.google.com/uc?export=view&id=1kCLyqH2tJE9NHNmGGos2x6VKfd6qZTKS",
+        descripton: "This is the fourth description of the image"
+    },
+    {
+        image: "https://drive.google.com/uc?export=view&id=1kLCiQ0K99pOb93kA0tN9C7gXtUorrb3X",
+        descripton: "This is the fifth description of the image"
+    }
+]
