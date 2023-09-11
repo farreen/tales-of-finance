e5de98975b2f20238edafc314c9b4e8d98d3ca64
Farah Nasreen <farahnasreen2000@gmail.com>
2023-09-11 13:39:43 +0530

    Impl custom buttons in infographic

--- src/modules/infographics/pages/listing.jsx
+++ src/modules/infographics/pages/listing.jsx
@@ -4,7 +4,7 @@ import Masonry from "react-masonry-css";
 import "../../../common/styles/global.css";
 import { Button, Card, Container, Modal } from "react-bootstrap";
 import '../styles/infographic-styles.css'
-
+import '../../../common/custom-button/custom-btn.css'
 const InfographicsListing = () => {
   const [openModal, setOpenModal] = useState(false);
   const modalHandler = () => setOpenModal(!openModal);
@@ -45,6 +45,17 @@ const InfographicsListing = () => {
                   longer.
                 </Card.Text>
               </Card.Body>
+              <div className="view-btn d-flex justify-content-center align-items-center gap-2 ">
+                view
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  height="1em"
+                  viewBox="0 0 448 512"
+                  fill="#ffffff"
+                >
+                  <path d="M438.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-160-160c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L338.8 224 32 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l306.7 0L233.4 393.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l160-160z" />
+                </svg>
+              </div>
             </Card>
           ))}
         </Masonry>
@@ -75,8 +86,10 @@ const InfographicsListing = () => {
                 lead-in to additional content. This content is a little bit
                 longer.
               </Card.Text>
+             
             </Card.Body>
           </Card>
+          
         </Modal.Body>
         <Modal.Footer>
           <Button onClick={modalHandler}>Close</Button>
